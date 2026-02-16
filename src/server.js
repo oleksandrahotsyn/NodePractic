00fa287333f7b5272b6eba64c379f4882faf53e7
Sandra Hotsyn <sandrahotsy@gmail.com>
2026-02-16 10:47:24 +0200

    install dotenv

--- src/server.js
+++ src/server.js
@@ -2,9 +2,10 @@
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
+import 'dotenv/config';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT ?? 3000;
 
 app.use(express.json());
 app.use(cors());
